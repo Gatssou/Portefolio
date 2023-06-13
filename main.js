56a0cece64d3c79dff1caf26ba908e1c542f9b09
Gatssou <87131331+Gatssou@users.noreply.github.com>
2023-06-13 13:49:14 +0200

    gfg
    gfg

--- main.js
+++ main.js
@@ -118,7 +118,7 @@ dracoLoader.setDecoderPath('draco/');
 const gltfLoader = new GLTFLoader();
 gltfLoader.setDRACOLoader(dracoLoader);
 gltfLoader.load(
-  'models/room7.glb',
+  'models/room9.glb',
   function (room) {
     // hide loader on loade
     loaderWrapper.style.display = 'none';
